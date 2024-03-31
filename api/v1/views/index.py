7d52608b40d75b8261b0563184704cba058e2925
HudaOmer <hudaoe175@gmail.com>
2024-03-31 16:38:51 -0700

    Solve task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@ from api.v1.views import app_views
 from flask import jsonify, request
 from models import storage
 
+
 @app_views.route('/status', methods=['GET'])
 def status():
     """
@@ -12,6 +13,7 @@ def status():
     if request.method == 'GET':
         response = {"status": "OK"}
         return jsonify(response)
+
 
 @app_views.route('/stats', methods=['GET'])
 def stats():
